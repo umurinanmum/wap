a6113fb257122c1d5b265c97bf8ff06d943e1654
umur inan <umurinan@gmail.com>
2019-01-30 09:13:03 -0600

    note controller addded

--- FinalProject/src/main/java/com/wap/controller/NoteController.java
+++ FinalProject/src/main/java/com/wap/controller/NoteController.java
@@ -1,4 +1,47 @@
 package com.wap.controller;
 
-public class NoteController {
+import com.wap.dto.NoteDto;
+import com.wap.enums.StatusCode;
+import com.wap.helper.JsonHelper;
+import com.wap.model.WapResultData;
+import com.wap.service.NoteService;
+import lombok.var;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet("/Note")
+public class NoteController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws  IOException {
+        String idTaskStr = req.getParameter("idTask");
+        if (idTaskStr == null || idTaskStr.trim().isEmpty()) {
+            var result = new WapResultData<ArrayList<NoteDto>>();
+            result.setStatusCode(StatusCode.MISSING_PARAMETER);
+            var resultJson = JsonHelper.toJson(result);
+            resp.getWriter().write(resultJson);
+            return;
+        }
+
+        int idTask = 0;
+        try {
+            idTask = Integer.parseInt(idTaskStr);
+        } catch (Exception e) {
+            var result = new WapResultData<ArrayList<NoteDto>>();
+            result.setStatusCode(StatusCode.INVALID_PARAMETER);
+            var resultJson = JsonHelper.toJson(result);
+            resp.getWriter().write(resultJson);
+            return;
+        }
+
+        NoteService noteService = new NoteService();
+        var result = noteService.getNotesByIdTask(idTask);
+        var resultJson = JsonHelper.toJson(result);
+        resp.getWriter().write(resultJson);
+    }
 }
--- FinalProject/src/main/java/com/wap/enums/StatusCode.java
+++ FinalProject/src/main/java/com/wap/enums/StatusCode.java
@@ -2,5 +2,7 @@ package com.wap.enums;
 
 public enum  StatusCode {
     SUCCESS,
+    MISSING_PARAMETER,
+    INVALID_PARAMETER,
     ERROR
 }
